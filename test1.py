fbd9249488983a54a1697c91d0e63ac1a0b084a2
huxing <787882003@qq.com>
2021-07-13 23:16:01 +0800

    app自动化练习

---
+++ test1.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from appium import webdriver
+caps={
+    'platformName':'Android',
+    'platformVersion':'10',
+    'deviceName':'MKB4C20619029439',
+    'appPackage':' com.shuixin.sns.test',
+    'appActivity':'com.shuixin.sns.SplashActivity',
+    'noReset':True,
+    'unicodeKeyboard':True,
+    'resetKeyboard':True}
+driver=webdriver.Remote('http://127.0.0.1:4723/wd/hub',caps)
+
+
+
+
